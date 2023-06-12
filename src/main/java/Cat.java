19722cdef36b3f6480cde583b97bb26df59f1ee5
yungbrave <SergeyIlinR@gmail.com>
2023-06-12 22:21:51 +0300

    first commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    String voice;
+
+    public String getVoice() {
+        return voice;
+    }
+
+    public void setVoice(String voice) {
+        this.voice = voice;
+    }
+}
